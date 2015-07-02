1b0d3334f08d454d9f081db3de8dbcc1e55c43dd
Ash Islam <ashislam858@gmail.com>
2015-07-02 10:20:48 -0400

    add postgres lib to setup

--- moopyweb/views.py
+++ moopyweb/views.py
@@ -8,13 +8,18 @@ from .models import (
     MyModel,
     )
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
     except DBAPIError:
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
     return {'one': one, 'project': 'moopy'}
+
+@view_config(route_name='jsontest', renderer='json')
+def jsontest(request):
+    return {'mood': ['anxious', 'upset'], 'intensity': 0.9}
+
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
